320708fe246a4ed26436bbfdbefd154786e666f8
Song Chuanqi <73341081+freedom3219@users.noreply.github.com>
2021-07-16 22:14:48 +0800

    Update PAT-B1003.我要通过！.java

--- PAT/javaSrc/PAT-B1003.我要通过！.java
+++ PAT/javaSrc/PAT-B1003.我要通过！.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        while (n -- > 0) {
+        while (n-- > 0) {
             String s = in.next();
             char[] str = s.toCharArray();
             int a = 0, b = 0, c = 0;
             int j = 0;
             boolean flag = false;
-            //a的长度
+            // a的长度
             while (str[j++] != 'P') {
                 if (j == str.length || (str[j-1] != ' ' && str[j-1] != 'A')) {
                     flag = true;
@@ -22,7 +22,7 @@ public class Main {
                 System.out.println("NO");
                 continue;
             }
-            //b的长度
+            // b的长度
             while (str[j++] != 'T') {
                 if (j == str.length || (str[j-1] != ' ' && str[j-1] != 'A')) {
                     flag = false;
@@ -34,9 +34,9 @@ public class Main {
                 System.out.println("NO");
                 continue;
             }
-            //c的长度
+            // c的长度
             c = str.length-j;
-            //输出
+            // 输出
             if (b == 0) {
                 System.out.println("NO");
             } else if (a * b == c) {
